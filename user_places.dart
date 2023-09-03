21ed30a96eff36aa6742b71aa3342767bb856c1a
Tanmay Kaushik <96286116+tanmaykaushikk@users.noreply.github.com>
2023-09-03 16:05:41 +0530

    Add files via upload
    THIS APPLICATION IS NOT FULLY COMPLETED YET .

---
+++ user_places.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'dart:io';
+
+import 'package:favourite_places/models/place.dart';
+
+class UserPlacesNotifier extends StateNotifier<List<Place>> {
+  UserPlacesNotifier() : super(const []);
+
+  void addPlace(String title , File image) {
+    final newPlace = Place(title: title , image: image);
+    state = [newPlace, ...state];
+  }
+}
+
+final userPlacesProvider =
+    StateNotifierProvider<UserPlacesNotifier, List<Place>>(
+  (ref) => UserPlacesNotifier(),
+);
